7583079cfa645e3228f1851eba4650a4cc1d9ad4
AsterLaoWhy <sboy99990@gmail.com>
2022-09-28 22:54:52 +0800

    vercel

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xAE0A82845Ad93A0B6db882b6A069e93b8185c48f";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
